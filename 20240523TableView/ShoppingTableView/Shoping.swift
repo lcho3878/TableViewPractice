461b3f87b689a754b3408908e806e2563688297d
lcho3878 <leech3878@naver.com>
2024-05-24 16:11:31 +0900

    Shopping 구조체 추가 및 구조 변경

---
+++ 20240523TableView/ShoppingTableView/Shoping.swift
@@ -0,0 +1,8 @@
+//
+//  Shoping.swift
+//  20240523TableView
+//
+//  Created by 이찬호 on 5/24/24.
+//
+
+import Foundation
---
+++ 20240523TableView/TodoTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TodoTableViewCell.swift
+//  20240523TableView
+//
+//  Created by 이찬호 on 5/24/24.
+//
+
+import UIKit
+
+class TodoTableViewController: UITableViewController {
+
+    
+    @IBOutlet var addTextField: UITextField!
+    @IBOutlet var addButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
